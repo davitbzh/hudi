df728bdf73f6dbc6863d2cb30b718d076afc217c
davitbzh <44586065+davitbzh@users.noreply.github.com>
2020-10-07 17:26:37 +0200

    Update KeyGenerator.java

--- hudi-spark/src/main/java/org/apache/hudi/keygen/KeyGenerator.java
+++ hudi-spark/src/main/java/org/apache/hudi/keygen/KeyGenerator.java
@@ -41,7 +41,7 @@ public abstract class KeyGenerator implements Serializable, KeyGeneratorInterfac
   private static final String STRUCT_NAME = "hoodieRowTopLevelField";
   private static final String NAMESPACE = "hoodieRow";
 
-  protected TypedProperties config;
+  protected transient TypedProperties config;
   private transient Function1<Object, Object> converterFn = null;
 
   protected KeyGenerator(TypedProperties config) {
